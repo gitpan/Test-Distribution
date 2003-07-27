2f763b29cad3275f59e0fbc28d3b82c607b1f0be
Marcel Grünauer <marcel@cpan.org>
2003-07-27 17:13:21 +0000

    Import of MARCEL/Test-Distribution-1.04 from CPAN.
    gitpan-cpan-distribution: Test-Distribution
    gitpan-cpan-version:      1.04
    gitpan-cpan-path:         MARCEL/Test-Distribution-1.04.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Test/Distribution.pm
+++ lib/Test/Distribution.pm
@@ -2,87 +2,274 @@ package Test::Distribution;
 
 use strict;
 use warnings;
-use File::Find::Rule;
-use Test::Pod;
 use Test::More;
 
-our $VERSION = '1.02';
+our $VERSION = '1.04';
+
+# our @types = qw/pod use versions description manifest prereq exports/;
+our @types = qw/pod use versions description prereq/;
+
+my @error;
+for (qw/File::Spec File::Basename File::Find::Rule/) {
+	eval "require $_";
+	push @error => $_ if $@;
+}
+if (@error) {
+	# construct a nice message with proper placement of commas and
+	# the verb 'to be' in the enumeration of the error(s)
+
+	@error = sort @error;
+	my $is = @error == 1 ? 'is' : 'are';
+	my $last = pop @error;
+	my $msg = join ', ' => @error;
+	$msg .= ' and ' if length $msg;
+	$msg .= "$last $is required for Test::Distribution";
+	plan skip_all => $msg;
+	exit;
+}
+
+# This runs during BEGIN
 
 sub import {
 	return if our $been_here++;
 	my $pkg = shift;
 	my %args = @_;
-	$args{only} ||= [ qw/pod use versions/ ];
-	$args{only} = ref $args{only} eq 'ARRAY'
-	    ? $args{only} : [ $args{only} ];
+
+	our @types;
+	$args{only} ||= \@types;
+	$args{only} = [ $args{only} ] unless ref $args{only} eq 'ARRAY';
+
+	$args{not} ||= [];
+	$args{not} = [ $args{not} ] unless ref $args{not} eq 'ARRAY';
 
 	$args{tests} ||= 0;
+
+	$args{dirlist} = [ qw(blib lib) ];
+	$args{dir}   ||= File::Spec->catfile(@{ $args{dirlist} });
+
 	run_tests(\%args);
 }
+
+# This runs after CHECK, i.e. at run-time
 
 sub run_tests {
 	my $args = shift;
 	my %args = %$args;
 
-	our @files = File::Find::Rule->file()->name('*.pm')->in('blib/lib');
+	our @files = File::Find::Rule->file()->name('*.pm')->in($args{dir});
 	our @packages = map {
-	    (my $x = $_) =~ s|^blib/lib/||;
-	    $x =~ s|/|::|g;
-	    $x =~ s|\.pm$||;
-	    $x } @files;
+	    # $_ is like 'blib/lib/Foo/Bar/Baz.pm',
+	    # after splitpath: $dir is 'blib/lib/Foo/Bar', $file is 'Baz.pm',
+	    # after splitdir: @dir is qw(blib lib Foo Bar),
+	    # after shifting off @{$args{dirlist}}, @dir is qw(Foo Bar),
+	    # so now we can portably construct the package name.
 
-	my %tests = (
-	    pod      => scalar @files,
-	    use      => scalar @packages,
-	    versions => scalar @packages,
-	);
+	    my ($vol, $dir, $file) = File::Spec->splitpath($_);
+	    my @dir = grep { length } File::Spec->splitdir($dir);
+	    shift @dir for @{ $args{dirlist} };
+	    join '::' => @dir, File::Basename::basename($file, '.pm');
+	    } @files;
 
 	my %perform = map { $_ => 1 } @{$args{only}};
+	delete @perform{ @{$args{not}} };
 
 	# need to use() modules before we can check their $VERSIONS,
 	# so we might as well test with use_ok().
 
 	$perform{use} = 1 if $perform{versions};
 
+	our %testers;
 	our $tests = $args{tests};
 	for my $type (keys %perform) {
-		die "no such test type: $type\n" unless exists $tests{$type};
-		$tests += $tests{$type};
+		die "no such test type: $type\n"
+		    unless grep /^$type$/ => our @types;
+
+		my $pkg = __PACKAGE__ . '::' . $type;
+		$testers{$type} = $pkg->new(
+		    packages => \@packages,
+		    files    => \@files,
+		    dir      => $args{dir},
+		);
+
+		$tests += $testers{$type}->num_tests;
 	}
 
 	plan tests => $tests;
 
-	if ($perform{pod}) {
-		for my $file (@files) { pod_ok($file) }
-	}
-
-	if ($perform{use}) {
-		for my $class (@packages) { use_ok($class) }
-	}
-
-	if ($perform{versions}) {
-		for my $class (@packages) {
-			our $version;
-
-			my $this_version = do {
-			    no strict 'refs';
-			    ${"$class\::VERSION"}
-			};
-
-			unless (defined $version) {
-				$version = $this_version;
-				ok(defined($version),
-				    "$class defines a version");
-				next;
-			}
-			is($this_version, $version, "$class version matches");
-		}
+	for my $type (keys %perform) {
+		$testers{$type}->run_tests;
 	}
 }
 
-sub packages   { our @packages }
+sub packages  { our @packages }
 sub files     { our @files }
 sub num_tests { our $tests }
+
+
+package Test::Distribution::base;
+
+sub new {
+	my ($class, %args) = @_;
+	bless \%args, $class;
+}
+
+sub num_tests { 0 }
+sub run_tests {}
+
+
+package Test::Distribution::pod;
+use Test::More;
+our @ISA = 'Test::Distribution::base';
+
+sub num_tests { scalar @{ $_[0]->{files} } }
+
+sub run_tests { SKIP: {
+	my $self = shift;
+
+	eval {
+		require Test::Pod;
+		Test::Pod->import;
+	};
+	skip 'Test::Pod required for testing POD', $self->num_tests() if $@;
+
+	for my $file (@{ $self->{files} }) { pod_file_ok($file) }
+} }
+
+
+package Test::Distribution::use;
+use Test::More;
+our @ISA = 'Test::Distribution::base';
+
+sub num_tests { scalar @{ $_[0]->{packages} } }
+
+sub run_tests {
+	my $self = shift;
+	for my $package (@{ $self->{packages} }) { use_ok($package) }
+}
+
+
+package Test::Distribution::versions;
+use Test::More;
+our @ISA = 'Test::Distribution::base';
+
+sub num_tests { scalar @{ $_[0]->{packages} } }
+
+sub run_tests {
+	my $self = shift;
+
+	for my $package (@{ $self->{packages} }) {
+		our $version;
+
+		my $this_version = do {
+		    no strict 'refs';
+		    ${"$package\::VERSION"}
+		};
+
+		unless (defined $version) {
+			$version = $this_version;
+			ok(defined($version),
+			    "$package defines a version");
+			next;
+		}
+		is($this_version, $version, "$package version matches");
+	}
+}
+
+
+package Test::Distribution::description;
+use Test::More;
+our @ISA = 'Test::Distribution::base';
+
+sub num_tests { 4 }
+
+sub run_tests {
+	my $self = shift;
+	ok(-e, "$_ exists") for qw/Changes MANIFEST README Makefile.PL/;
+}
+
+
+# XXX - not yet implemented and no docs or tests yet.
+package Test::Distribution::manifest;
+use Test::More;
+our @ISA = 'Test::Distribution::base';
+
+sub num_tests { 0 }
+
+sub run_tests {
+	my $self = shift;
+}
+
+
+package Test::Distribution::prereq;
+use Test::More;
+our @ISA = 'Test::Distribution::base';
+
+sub num_tests { 1 }
+
+sub run_tests { SKIP: {
+	my $self = shift;
+
+	eval {
+		require File::Find::Rule;
+		require Module::CoreList;
+	};
+	skip 'File::Find::Rule and Module::CoreList required for testing PREREQ_PM', $self->num_tests() if $@;
+
+	my (%use, %package);
+
+	File::Find::Rule->file()->nonempty()->or(
+	    File::Find::Rule->name(qr/\.p(l|m|od)$/),
+	    File::Find::Rule->exec(sub {
+		my $fh;
+		return 0 unless open $fh, $_;
+		my $shebang = <$fh>;
+		close $fh;
+		return $shebang =~ /^#!.*\bperl/;
+	    }),
+	)->exec(sub {
+	    my $fh;
+	    return 0 unless open $fh, $_;
+	    while (<$fh>) {
+		    $use{$1}++ if /^use \s+ ([^\W\d][\w:]+) (\s*\n | .*;)/x;
+		    $package{$1}++ if /^package \s+ ([\w:]+) \s* ;/x;
+	    }
+	    return 1;
+	})->in($self->{dir});
+
+	# We're not interested in use()d modules that are provided by
+	# this distro, or in core modules. It's ok core modules aren't
+	# mentioned in PREREQ_PM.
+
+	no warnings 'once';
+	delete @use{ keys %package, keys %{ $Module::CoreList::version{$]} } };
+
+	open my $fh, 'Makefile.PL' or die "can't open Makefile.PL: $!\n";
+	my $make = do { local $/; <$fh> };
+	close $fh or die "can't close Makefile.PL: $!\n";
+	$make =~ s/use \s+ ExtUtils::MakeMaker \s* ;/no strict;/gx;
+
+	$make .= 'sub WriteMakefile {
+	    my %h = @_; our @prereq = keys %{ $h{PREREQ_PM} || {} } }';
+	eval $make;
+	die $@ if $@;
+
+	delete @use{our @prereq};
+	my $missing = join "\n" => keys %use;
+	is($missing, '', 'used packages in PREREQ_PM');
+} }
+
+
+# XXX - not yet implemented and no docs or tests yet.
+package Test::Distribution::exports;
+use Test::More;
+our @ISA = 'Test::Distribution::base';
+
+sub num_tests { 0 }
+
+sub run_tests {
+	my $self = shift;
+}
+
 
 1;
 
@@ -134,8 +321,8 @@ C<Test::Distribution>.
 =item C<only =E<gt> STRING|LIST>
 
 Specifies that only certain sets of tests are to be run. Possible values
-are C<pod>, C<use> and C<versions>. For example, if you only want to
-run the POD tests, you could say:
+are those mentioned in TEST TYPES below. For example, if you only want
+to run the POD tests, you could say:
 
   use Test::Distribution only => 'pod';
 
@@ -151,7 +338,80 @@ is automatically added. This is because in order to get a module's
 C<$VERSION>, it has to be loaded. In this case we might as well run a
 C<use> test.
 
+The value for C<only> can be a string or a reference to a list of strings.
+
+=item C<not =E<gt> STRING|LIST>
+
+Specifies that certain types of tests should not be run. All tests not
+mentioned in this argument are run. For example, if you want to test
+everything except the POD, use:
+
+  use Test::Distribution
+    not => 'pod';
+
+The value for C<not> can be a string or a reference to a list of
+strings. Although it doesn't seem to make much sense, you can use both
+C<only> and C<not>. In this case only the tests specified in C<only>,
+but not C<not> are run (if this makes any sense).
+
 =back
+
+=head1 TEST TYPES
+
+Here is a description of the types of tests available.
+
+=over 4
+
+=item C<pod>
+
+Checks for POD errors in files
+
+=item C<use>
+
+This C<use()>s the modules to make sure the load happens ok.
+
+=item C<versions>
+
+Checks that all packages define C<$VERSION> strings.
+
+=item C<description>
+
+Checks that the following files exist:
+
+=over 4
+
+=item Changes
+
+=item MANIFEST
+
+=item README
+
+=item Makefile.PL
+
+=back
+
+=item C<prereq>
+
+Checks whether all C<use()>d modules that aren't in the perl core are
+also mentioned in Makefile.PL's C<PREREQ_PM>.
+
+=back
+
+=head1 TEST::DISTRIBUTION'S OWN PREREQUISITES
+
+C<Test::Distribution> uses C<File::Find::Rule> and C<Test::Pod>, amongst
+other modules. Each of these modules have their own rather long list
+of prerequisites. If you use the CPAN shell or a package manager to
+install modules, this is probably of no concern, but if you install
+modules manually, you might not care to install a lot of modules just
+to get C<Test::Distribution> to run.
+
+Because of this, C<Test::Distribution> checks whether you have
+the required modules installed and skips tests (per C<Test::More>
+definition) as necessary if these modules are not found. If you don't
+have C<Test::Pod>, you can't run the C<pod> tests. If you don't have
+C<Module::CoreList>, you can't run the C<prereq> tests. But if you don't
+have C<File::Find::Rule>, all tests are skipped.
 
 =head1 EXPOSED INTERNALS
 
@@ -181,6 +441,24 @@ specifications.
 
 =back
 
+=head1 FEATURE IDEAS
+
+=over 4
+
+=item C<manifest> test type
+
+This would check the MANIFEST's integrity.
+
+=item C<export> test type
+
+This would mandate that there should be a test for each exported symbol
+of each module.
+
+=back
+
+Let me know what you think of these ideas. Are they
+necessary? Unnecessary? Do you have feature requests of your own?
+
 =head1 BUGS
 
 If you find any bugs or oddities, please do inform the author.
@@ -197,7 +475,7 @@ site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
 
 =head1 VERSION
 
-This document describes version 1.02 of C<Test::Distribution>.
+This document describes version 1.04 of C<Test::Distribution>.
 
 =head1 AUTHOR
 
@@ -205,20 +483,20 @@ Marcel GrE<uuml>nauer <marcel@cpan.org>
 
 =head1 OTHER CREDITS
 
-This module was inspired by a use.perl.org journal entry by brian d foy
+This module was inspired by a use.perl.org journal entry by C<brian d foy>
 (see http://use.perl.org/~brian_d_foy/journal/7463) where he describes
 an idea by Andy Lester.
 
 =head1 COPYRIGHT
 
-Copyright 2002 Marcel GrE<uuml>nauer. All rights reserved.
+Copyright 2002-2003 Marcel GrE<uuml>nauer. All rights reserved.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-perl(1), Test::More(3pm), Test::Pod(3pm).
+perl(1), Test::More(3pm), Test::Pod(3pm), File::Find::Rule(3pm).
 
 =cut
 
--- t/02extra.t
+++ t/02extra.t
@@ -2,8 +2,8 @@ use Test::Distribution tests => 4;
 use Test::More;
 ok(1, 'extra test');
 
-# 3 * (1 module) + 4 extra
-is(Test::Distribution::num_tests(), 7, 'number of tests');
+# 4 descriptions + 3 * (1 module) + 4 extra + 1 prereq
+is(Test::Distribution::num_tests(), 12, 'number of tests');
 
 is_deeply(Test::Distribution::packages(), 'Test::Distribution',
     'packages found');
